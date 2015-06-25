a288f859ad7b5b12ccda589abedd87f0a2f7dfc7
L Chen <jenac@outlook.com>
2015-06-25 09:03:15 -0500

    adding internet ip

--- InvestorsAssist/InvestorsAssist._OnStart/Program.cs
+++ InvestorsAssist/InvestorsAssist._OnStart/Program.cs
@@ -5,7 +5,9 @@ using InvestorsAssist.Utility.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -31,7 +33,7 @@ namespace InvestorsAssist._OnStart
                             new List<string>(),
                             string.Format("IA: Computer {0} started @ {1}",
                                 Environment.MachineName, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")),
-                            string.Empty);
+                            string.Format("IP: <b>{0}</b>", GetInternetIP()));
                     }
                 }
                 catch (Exception e)
@@ -40,12 +42,27 @@ namespace InvestorsAssist._OnStart
                     Logger.Instance.Error(e.StackTrace);
                     Thread.Sleep(30 * 1000);
                     continue;
-                    
+
                 }
                 break;
             }
 
             Logger.Instance.Info("InvestorsAssist._OnStart done.");
+        }
+
+        static string GetInternetIP()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    var externalIP = client.DownloadString("http://checkip.dyndns.org/");
+                    var reg = new Regex(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
+                    externalIP = reg.Matches(externalIP)[0].ToString();
+                    return externalIP;
+                }
+            }
+            catch { return string.Empty; }
         }
     }
 }
