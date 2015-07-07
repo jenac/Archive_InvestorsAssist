8d7f4d89d99acc917b8e40a9aaf997ad28f85aa8
L Chen <jenac@outlook.com>
2015-07-07 10:39:02 -0500

    Add _WebApi

---
+++ InvestorsAssist/InvestorsAssist._WebApi/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestorsAssist._WebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string baseAddress = @"http://localhost:8888/InvestorsAssist/";// ConfigurationManager.AppSettings["BaseAddress"];
+                if (string.IsNullOrEmpty(baseAddress))
+                    throw new Exception("Missing Configuration: BaseAddress");
+                Console.WriteLine("Starting service...");
+                using (WebApp.Start<Startup>(baseAddress))
+                {
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+}
---
+++ InvestorsAssist/InvestorsAssist._WebApi/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using System;
+using System.Configuration;
+using System.Web.Http;
+
+namespace InvestorsAssist._WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.Routes.IgnoreRoute("JS", "js/{*pathInfo}");
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            appBuilder.UseWebApi(config);
+
+            /*
+            // Configure Static Contents, map js to root
+            var fileSystem = new PhysicalFileSystem(@".\js");
+            var options = new FileServerOptions()
+            {
+                EnableDirectoryBrowsing = true,
+                FileSystem = fileSystem,
+            };
+
+            appBuilder.UseFileServer(options);*/
+
+        }
+    }
+}
