ea3a4cdafe2597986cfe409a877aa6eca827cfaa
L Chen <jenac@outlook.com>
2015-07-17 11:07:27 -0500

    Add SharpChart

---
+++ InvestorsAssist/InvestorsAssist.WebApi/Controllers/SharpChartController.cs
@@ -0,0 +1,47 @@
+﻿using InvestorsAssist.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace InvestorsAssist.WebApi.Controllers
+{
+    public class SharpChartController : ApiController
+    {
+        // GET api/sharpchart/AAPL
+        public SharpChartJS Get(string id)
+        {
+            try
+            {
+                SharpChartJS value = new SharpChartJS
+                {
+                    DailyChartBase64 = GetStockChartsImage("D", id),
+                    WeeklyChartBase64 = GetStockChartsImage("W", id),
+                };
+                return value;
+            }
+            catch
+            {
+                return new SharpChartJS();
+            }
+        }
+
+
+        //period:
+        //D - daily
+        //W - weekly
+        private string GetStockChartsImage(string period, string symbol)
+        {
+            byte[] imageAsByteArray;
+            using (var webClient = new WebClient())
+            {
+                imageAsByteArray = webClient.DownloadData(
+                    string.Format("http://stockcharts.com/c-sc/sc?s={0}&p={1}&b=5&g=0&i=0", symbol, period));
+                return Convert.ToBase64String(imageAsByteArray);
+            }
+
+        }
+    }
+}
---
+++ InvestorsAssist/InvestorsAssist.WebApi/Models/SharpChartJS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestorsAssist.WebApi.Models
+{
+    public class SharpChartJS
+    {
+        public string DailyChartBase64 { get; set; }
+        public string WeeklyChartBase64 { get; set; }
+    }
+}
