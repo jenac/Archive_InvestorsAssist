ce6a19bda1bacd7ec291f9c36ac430d180e19890
L Chen <jenac@outlook.com>
2015-07-24 10:28:42 -0500

    add yo generated

---
+++ InvestorsAssist/InvestorsAssist.Js/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name investorsAssistjsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the investorsAssistjsApp
+ */
+angular.module('investorsAssistjsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ InvestorsAssist/InvestorsAssist.Js/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name investorsAssistjsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the investorsAssistjsApp
+ */
+angular.module('investorsAssistjsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
